c47427944add2bff871e2ef1766dc33c4b18c153
Zayd Dawood <zdawood@glos.ac.uk>
2024-03-11 13:25:21 +0000

    Added Show and Hide a GameObject

---
+++ Assets/Samples/Google Cardboard XR Plugin for Unity/1.23.0/Hello Cardboard/Scripts/PlayerInteractions.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+
+    private int distanceOfRaycast = 10;
+    
+    private RaycastHit _hit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+
+        if (Physics.Raycast(ray, out _hit, distanceOfRaycast))
+        {
+
+            if (Input.GetButton("Fire1") && _hit.transform.CompareTag("Rotatable"))
+            {
+
+                _hit.transform.gameObject.GetComponent<RotateObject>().ChangeSpin();
+
+            }
+
+            if (Input.GetButton("Fire1") && _hit.transform.CompareTag("ShowHideTrigger"))
+            {
+
+                _hit.transform.gameObject.GetComponent<ShowHideObject>().ChangeActive();
+
+            }
+
+        }
+
+    }
+}
